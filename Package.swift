a97e1e577534ac630f2793751745668b994595af
Benjamin Horsleben <benjamin@fizkerinc.dk>
2024-12-22 08:12:59 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let upcomingFeatures: [SwiftSetting] = [
+	.enableUpcomingFeature("ExistentialAny"),
+	.enableExperimentalFeature("StrictConcurrency"),
+	.enableExperimentalFeature("AccessLevelOnImport"),
+	.enableUpcomingFeature("InternalImportsByDefault"),
+	.enableUpcomingFeature("FullTypedThrows"),
+]
+
+let package = Package(
+	name: "tunnels-models",
+	products: [
+		.library(
+			name: "TunnelModels",
+			targets: ["TunnelModels"]
+		),
+	],
+	targets: [
+		.target(
+			name: "LogModels",
+			dependencies: [
+				"TunnelModels"
+			],
+			swiftSettings: upcomingFeatures
+		),
+		.target(
+			name: "TunnelModels",
+			swiftSettings: upcomingFeatures
+		),
+		.testTarget(
+			name: "TunnelModelsTests",
+			dependencies: ["TunnelModels"],
+			swiftSettings: upcomingFeatures
+		),
+		.testTarget(
+			name: "LogModelsTests",
+			dependencies: ["LogModels"],
+			swiftSettings: upcomingFeatures
+		),
+	]
+)
